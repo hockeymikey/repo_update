352eae3b5d4c33b2aab3ea0d24cfc39a2ca54a90
russel5 <rnuriyev82@gmail.com>
2018-04-30 00:49:16 +0500

    Release

---
+++ repo_update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ANDROOT=$PWD
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+repo sync -j8 --current-branch --no-tags
+
+pushd $ANDROOT/frameworks/opt/net/wifi
+git fetch https://github.com/LineageOS/android_frameworks_opt_net_wifi refs/changes/26/206926/3 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/bionic
+git fetch https://github.com/LineageOS/android_bionic refs/changes/70/206170/2 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/build/make
+git fetch https://github.com/LineageOS/android_build refs/changes/71/206171/2 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/device/rr/sepolicy
+git fetch https://github.com/LineageOS/android_device_lineage_sepolicy refs/changes/97/206797/5 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/hardware/qcom/display-caf/msm8998
+git revert --no-edit -m 1 6ecb2ed62fefc3efee5ec57539dec4e788ad157b
+popd
