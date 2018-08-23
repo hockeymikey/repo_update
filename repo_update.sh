18334500daaf72e306045ad18419cc700bfb8559
russel5 <rnuriyev82@gmail.com>
2018-08-23 21:15:41 +0500

    add sepolicy patch
    Change-Id: Id3d9a466ffb6194434c57ae331c68dad1e4412a0

--- repo_update.sh
+++ repo_update.sh
@@ -25,5 +25,14 @@ git fetch https://github.com/LineageOS/android_build refs/changes/71/206171/2 &&
 popd
 
 pushd $ANDROOT/device/rr/sepolicy
-git fetch https://github.com/LineageOS/android_device_lineage_sepolicy refs/changes/27/212927/2 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_device_lineage_sepolicy refs/changes/27/212927/7 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_device_lineage_sepolicy refs/changes/87/225587/1 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/packages/apps/Snap
+git fetch https://github.com/LineageOS/android_packages_apps_Snap refs/changes/16/225516/2 && git cherry-pick FETCH_HEAD
+popd
+
+pushd $ANDROOT/packages/apps/Gallery2
+git fetch https://github.com/LineageOS/android_packages_apps_Gallery2 refs/changes/86/225586/1 && git cherry-pick FETCH_HEAD
 popd
