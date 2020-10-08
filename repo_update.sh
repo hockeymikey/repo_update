c36a63720581eee3402cd6663761f7c04e9d06ae
hockeymikey <hockeymikey@hockeymikey.com>
2020-10-08 15:28:56 -0500

    Update repo_update.sh
    No idea what the point of those are, remove for now.

--- repo_update.sh
+++ repo_update.sh
@@ -15,13 +15,13 @@ repo sync -j$(nproc) --current-branch --no-tags --force-sync
 fi
 
 pushd $ANDROOT/frameworks/opt/net/wifi
-git fetch https://github.com/LineageOS/android_frameworks_opt_net_wifi refs/changes/26/206926/3 && git cherry-pick FETCH_HEAD
+#git fetch https://github.com/LineageOS/android_frameworks_opt_net_wifi refs/changes/26/206926/3 && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/bionic
-git fetch https://github.com/LineageOS/android_bionic refs/changes/70/206170/2 && git cherry-pick FETCH_HEAD
+#git fetch https://github.com/LineageOS/android_bionic refs/changes/70/206170/2 && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/build/make
-git fetch https://github.com/LineageOS/android_build refs/changes/71/206171/2 && git cherry-pick FETCH_HEAD
+#git fetch https://github.com/LineageOS/android_build refs/changes/71/206171/2 && git cherry-pick FETCH_HEAD
 popd
